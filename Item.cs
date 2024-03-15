8180f5b105cfd7d32a264937e9687f08b11c39c5
Stoszek <153231720+Stoszek@users.noreply.github.com>
2024-03-15 16:07:30 +0100

    Add files via upload

---
+++ Item.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wyrzykowski_item
+{
+    internal class Item
+    {
+        public int Value = 0;
+        public int Weight = 0;
+        public double ValueByWeight;
+
+        public Item(int value, int weight)
+        {
+            Value = value;
+            Weight = weight;
+            ValueByWeight = value/weight;
+        }
+
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿namespace Wyrzykowski264228
+{
+    
+}
